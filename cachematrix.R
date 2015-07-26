fb34837f7d836b698f7b29a0c556dc37c1262598
marcocaldascruz <marcocaldascruz@gmail.com>
2015-07-26 05:42:00 -0500

    Assignment2
    Cache Function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## This function sets the values (matrix and inverse matrix values).
 makeCacheMatrix <- function(x = matrix()) {
 
+## Init the value of m, its will be the inverse value of the matrix.
+
+            m <- NULL
+
+## it implements the “set” Function which sets the value of the matrix.
+## the m value is NULL (yet).
+
+            set <- function(y) {
+                    x <<- y
+                    m <<- NULL
+            }
+## it implements the “get” Function which returns the value of the matrix.
+
+            get <- function() x
+
+## it implements the “setsolve” Function which sets the inverse of the matrix.
+            setsolve <- function(solve) m <<- solve
+
+## it implements the “getsolve” Function which returns the inverse of the matrix.
+            getsolve <- function() m
+
+## The functions list associates to the makeCacheMatrix function.
+            list(set = set, get = get,
+                 setsolve = setsolve,
+                 getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## This function returns the inverse matrix value cached or it computes.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## In variable m, the function deposits the value of the inverse of the matrix.
+            m <- x$getsolve()
+
+## If the value is defined then the function only shows the variable (any compute).
+            if(!is.null(m)) {
+                    message("getting cached data")
+                    return(m)
+            }
+## else in “data” gets the value of the matrix.
+            data <- x$get()
+##      in “m” gets the value of the inverse of the matrix.
+            m <- solve(data, ...)
+##      it sets in m, the value calculated (inverse of the matrix).
+            x$setsolve(m)
+##      return m (inverse matrix value).
+            m
+    
 }
